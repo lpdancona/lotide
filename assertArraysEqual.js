10a687f7c5bc5123e72bd661e02529ac9400353f
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-10 18:46:57 -0800

    without function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
 const assertEqual = function (actual, expected) {
-  if (JSON.stringify(actual) == JSON.stringify(expected))
+  if (JSON.stringify(actual) === JSON.stringify(expected))
     console.log(`Assertion Passed: ${actual} does match ${expected}`);
   else console.log(`Assertion Failed: ${actual} does not match ${expected}`);
 };
---
+++ without.js
@@ -0,0 +1,18 @@
+const without = function (source, itemsToRemove) {
+  let newArr = [];
+
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemsToRemove[i]) {
+      newArr.push(source[i]);
+    }
+  }
+  return newArr;
+};
+const assertEqual = function (actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected))
+    console.log(`Assertion Passed: ${actual} does match ${expected}`);
+  else console.log(`Assertion Failed: ${actual} does not match ${expected}`);
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
