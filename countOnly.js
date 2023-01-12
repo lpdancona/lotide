ab237b20da1e635e507ab0770d13a5d651b9cc8a
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-11 16:25:35 -0800

    conutOnly function

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countOnly = function (allItems, itemsToCount) {
+  let result = {};
+  for (let i = 0; i <= allItems.length; i++) {
+    // console.log(allItems[i]);
+    let key = allItems[i];
+    if (itemsToCount[key]) {
+      if (result[key]) {
+        result[key] += 1;
+      } else {
+        result[key] = 1;
+      }
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
