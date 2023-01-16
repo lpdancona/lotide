e2e873555555d24218d4ad31debadeb748f56a37
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-16 13:21:39 -0800

    creatinf test for tail

--- tail.js
+++ tail.js
@@ -9,8 +9,4 @@ const tail = function (arr) {
   const copyWithoutFirst = arr.slice(1);
   return copyWithoutFirst;
 };
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail(["word", "lettuce", "burguer"]));
+module.exports = tail;
