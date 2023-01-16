08ff76440459cfe0d1b12247e1ab638c1ac8ec79
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-16 14:33:17 -0800

    implementing chai and mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,18 @@
+const assert = require("chai").assert;
 const middle = require("../middle");
 const assertArraysEqual = require("../assertArraysEqual");
-console.log(middle([1]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
+// console.log(middle([1]));
+// console.log(middle([1, 2, 3]));
+// console.log(middle([1, 2, 3, 4, 5, 6]));
+
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert(middle([1, 2, 3]), [2]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
+const assert = require("chai").assert;
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail(["word", "lettuce", "burguer"]));
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+// console.log(tail(["word", "lettuce", "burguer"]));
+describe("#tail", () => {
+  it("returns [Lighthouse, Labs] for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+  it("returns [lettuce, burguer] for [word, lettuce, burguer]", () => {
+    assert.deepEqual(tail(["word", "lettuce", "burguer"]), [
+      "lettuce",
+      "burguer",
+    ]);
+  });
+});
